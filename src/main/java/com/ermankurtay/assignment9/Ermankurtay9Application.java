6592ab4f7bb78ce158addb75103b69a2777a4092
ermankrty <kurtayerman@gmail.com>
2024-02-04 17:40:34 -0500

    assignment9

---
+++ src/main/java/com/ermankurtay/assignment9/Ermankurtay9Application.java
@@ -0,0 +1,13 @@
+package com.ermankurtay.assignment9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ermankurtay9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ermankurtay9Application.class, args);
+	}
+
+}
